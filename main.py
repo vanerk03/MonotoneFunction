58a9aced773026cc6951ef923b406ba072cae975
vanerk03 <63353271+vanerk03@users.noreply.github.com>
2021-10-19 19:44:11 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# time complexity of the algorithm is O(k * log k) = O(k * n)
+# k = 2 ** n
+# n - number of arguments boolean fucntion takes
+
+def isMonotone(n: int, vector: str) -> bool:
+
+    used = [False] * (2 ** n)
+
+    def dfs(s):
+
+        res = True
+        copyS = s
+        used[s] = True
+        now = 1
+
+        for _ in range(n):
+
+            bit = copyS % 2
+            copyS //= 2
+
+            if bit == 0:
+                if int(vector[now + s]) < int(vector[s]):
+                    return False
+                if not used[now + s]:
+                    res = res and dfs(now + s)
+
+            now *= 2
+        return res
+
+    return dfs(0)
